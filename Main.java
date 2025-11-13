45412a15d49b0d8bd57b7a651395ece1fdcea061
Rocio <rocioromerod98.com>
2025-11-13 18:20:02 +0100

    commit 3

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Versión inicial del proyecto.");
         System.out.println("Funcionalidad de login implementada.");
+        System.out.println("Funcionalidad de registro implementada.");
+
 
     }
 }
